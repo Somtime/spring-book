590811b98d5ac29ec4e5a2ff2b2d17578512f981
Yu <geonhee0821@naver.com>
2022-03-18 20:11:52 +0900

    2장 테스트

---
+++ spring/src/test/java/learningtest/JUnitTest.java
@@ -0,0 +1,32 @@
+package learningtest;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class JUnitTest {
+    static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+
+    @Test
+    public void test1() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+
+    @Test
+    public void test2() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+
+    @Test
+    public void test3() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+}
